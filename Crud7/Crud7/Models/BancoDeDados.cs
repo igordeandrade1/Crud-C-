425bd436f62339dac8049dceefcf76de494cf3d6
igordeandrade1 <116108662+igordeandrade1@users.noreply.github.com>
2024-06-07 02:18:35 -0300

    commit1

---
+++ Crud7/Crud7/Models/BancoDeDados.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Crud7.Models
+{
+    public class BancoDeDados : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=Crud7;Integrated Security=True");
+
+        }
+    }
+}
---
+++ Crud7/Crud7/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Crud7.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
